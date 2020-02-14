6eda1870a56929871e350fc6615e36ca31297270
JanssensMaxime <60343228+JanssensMaxime@users.noreply.github.com>
2020-02-14 16:12:39 +0100

    scrollbar app

--- src/App.vue
+++ src/App.vue
@@ -19,3 +19,46 @@
       }),
     };
 </script>
+
+<style>
+    ::-webkit-scrollbar {
+        height: 10px;
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-button {
+        height: 0px;
+        width: 0px;
+    }
+
+    ::-webkit-scrollbar-corner {
+        background: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: blueviolet;
+        border: 0px none blueviolet;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: blueviolet;
+        filter: grayscale(50%);
+    }
+
+    ::-webkit-scrollbar-thumb:active {
+        background: blueviolet;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #013F52;
+        border: 0px none #013F52;
+    }
+
+    ::-webkit-scrollbar-track:hover {
+        background: #013F52;
+    }
+
+    ::-webkit-scrollbar-track:active {
+        background: #013F52;
+    }
+</style>
