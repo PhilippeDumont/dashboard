1d423aaf2edc387af7f6b0bb0c02ca0e16ce1b92
Sean PRÉVOT <la170463@student.helha.be>
2020-02-20 15:09:43 +0100

    modifications onto the creation of the db

--- api/import_in_database/create_project.py
+++ api/import_in_database/create_project.py
@@ -2,13 +2,17 @@ import sqlite3
 import logging
 from datetime import date
 from database import init_database_activities_items
+from methods_on_project_database import project_exist
 
 
 def run(project_name):
     project_id = None
     try:
-        _create_new_project(project_name)
-        project_id = init_database_activities_items.run(project_name)
+        if not project_exist.run(project_name):
+            _create_new_project(project_name)
+            project_id = init_database_activities_items.run(project_name)
+        else:
+            return None
     except Exception as err:
         logging.error(err)
     return project_id
