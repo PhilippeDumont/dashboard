6f68d89a93214285c2253c9dd0cba4f7b0c6da9f
Sean PRÉVOT <la170463@student.helha.be>
2020-02-27 14:58:22 +0100

    Added commentary

--- api/tests/test6_update_opening_date.py
+++ api/tests/test6_update_opening_date.py
@@ -10,11 +10,14 @@ project_id = 1
 
 class TestUpdateDatabaseProject(unittest.TestCase):
     def test_update_last_opening_date_project(self):
+        # Update the project
         update_project_last_opening_date.run(project_id)
+        # Get the date back from the database
         conn = sqlite3.connect('../database_files/project_db/all_project.db')
         cursor = conn.cursor()
         query = """SELECT last_opening_date from projects WHERE id = ?"""
         cursor.execute(query, str(project_id))
+        # Get the data to check if is ok
         date_today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         date_str = cursor.fetchone()[0].split(".")
         self.assertEqual(date_str[0], date_today)
