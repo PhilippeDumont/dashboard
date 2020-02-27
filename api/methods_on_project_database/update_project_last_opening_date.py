cfaef874f00ccd31b4bd46219cd5a2d0407ce602
Sean PRÉVOT <la170463@student.helha.be>
2020-02-27 09:41:15 +0100

    added the data reopen date update and added the update for the number of items and activities in database

---
+++ api/methods_on_project_database/update_project_last_opening_date.py
@@ -0,0 +1,12 @@
+import sqlite3
+from datetime import date
+
+
+def run(project_id):
+    conn = sqlite3.connect('../database_files/project_db/all_project.db')
+    cursor = conn.cursor()
+    date_today = date.today().isoformat()
+    query = """UPDATE projects SET last_opening_date = ? WHERE id = ?"""
+    data = (date_today, str(project_id))
+    cursor.execute(query, data)
+    conn.commit()
