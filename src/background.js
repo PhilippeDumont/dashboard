6e8ee0bf0a88b5a81a93dec28d30b75a7360a9d6
Grisue <159852BnhjA>
2020-02-27 10:30:52 +0100

    background js correction

--- src/background.js
+++ src/background.js
@@ -176,11 +176,11 @@ ipcMain.on('open-dialog', (event) => {
 
 
 autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+  win.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+  win.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
