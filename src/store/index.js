a7d9beab3d54b925bdfbd059794c84e3829bfb1f
JanssensMaxime <60343228+JanssensMaxime@users.noreply.github.com>
2020-03-02 11:49:07 +0100

    dropdown show 3 projects sorted by last_opening_date

--- src/store/index.js
+++ src/store/index.js
@@ -4,50 +4,51 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    currentProject: null,
-    listProjects: [],
-    expansionBarVisibility: true
-  },
-  getters: {
-    getListProjects: state => state.listProjects,
-    getExpansionBarVisibility: state => state.expansionBarVisibility
-  },
-  mutations: {
-    /* Project */
-    ADD_PROJECT(state, newProject) {
-      state.listProjects.push(newProject)
-      state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+    state: {
+        currentProject: null,
+        listProjects: [],
+        expansionBarVisibility: true
     },
-    DELETE_PROJECT(state, project) {
-      state.listProjects.splice(state.listProjects.indexOf(project), 1)
-      console.log(state.listProjects)
+    getters: {
+        getListProjects: state => state.listProjects.sort(function(a, b) {
+            return new Date(b.last_opening_date) - new Date(a.last_opening_date);
+        }),
+        getExpansionBarVisibility: state => state.expansionBarVisibility
     },
-    SET_CURRENT_PROJECT(state, currentProject) {
-      state.currentProject = currentProject
+    mutations: {
+        /* Project */
+        ADD_PROJECT(state, newProject) {
+            state.listProjects.push(newProject)
+            state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+        },
+        DELETE_PROJECT(state, project) {
+            state.listProjects.splice(state.listProjects.indexOf(project), 1)
+            console.log(state.listProjects)
+        },
+        SET_CURRENT_PROJECT(state, currentProject) {
+            state.currentProject = currentProject
+        },
+        SET_LIST_PROJECTS(state, list) {
+            state.listProjects = list
+            state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+        },
+        SET_EXPANSION_BAR_VISIBILITY(state, visibility) {
+            state.expansionBarVisibility = visibility
+        }
     },
-    SET_LIST_PROJECTS(state, list) {
-      state.listProjects = list
-      state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+    actions: {
+        addProject({ commit }, project) {
+            commit('ADD_PROJECT', project)
+        },
+        deleteProject({ commit }, project) {
+            commit('DELETE_PROJECT', project)
+        },
+        setCurrentProject({ commit }, project) {
+            commit('SET_CURRENT_PROJECT', project)
+        },
+        setExpansionBarVisibility({ commit }, visibility) {
+            commit('SET_EXPANSION_BAR_VISIBILITY', visibility)
+        }
     },
-    SET_EXPANSION_BAR_VISIBILITY(state, visibility) {
-       state.expansionBarVisibility = visibility
-    }
-  },
-  actions: {
-    addProject({ commit }, project) {
-      commit('ADD_PROJECT', project)
-    },
-    deleteProject({ commit }, project) {
-      commit('DELETE_PROJECT', project)
-    },
-    setCurrentProject({ commit }, project) {
-      commit('SET_CURRENT_PROJECT', project)
-    },
-    setExpansionBarVisibility({ commit }, visibility) {
-      commit('SET_EXPANSION_BAR_VISIBILITY', visibility)
-    }
-  },
-  modules: {
-  }
+    modules: {}
 })
