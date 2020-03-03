344cb564e27a2eecbd2e33948cd98effe8e4449d
JanssensMaxime <60343228+JanssensMaxime@users.noreply.github.com>
2020-03-03 15:10:34 +0100

    snackbar as component + integration

--- src/store/index.js
+++ src/store/index.js
@@ -4,64 +4,95 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    currentProject: null,
-    listProjects: [],
-    expansionBarVisibility: true
-  },
-  getters: {
-    getListProjects: state => state.listProjects.sort(function (a, b) {
-      return new Date(b.last_opening_date) - new Date(a.last_opening_date);
-    }),
-    getExpansionBarVisibility: state => state.expansionBarVisibility
-  },
-  mutations: {
-    /* Project */
-    ADD_PROJECT(state, newProject) {
-      state.listProjects.push(newProject)
-      state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+    state: {
+        currentProject: null,
+        listProjects: [],
+        expansionBarVisibility: true,
+        msgSnackBar: "",
+        snackBarToShow: false,
+        colorSnackBar: ""
     },
-    DELETE_PROJECT(state, project) {
-      state.listProjects.splice(state.listProjects.indexOf(project), 1)
-      console.log(state.listProjects)
+    getters: {
+        getListProjects: state => state.listProjects.sort(function(a, b) {
+            return new Date(b.last_opening_date) - new Date(a.last_opening_date);
+        }),
+        getExpansionBarVisibility: state => state.expansionBarVisibility,
+        getMsgSnackBar: state => state.msgSnackBar,
+        getSnackBarToShow: state => state.snackBarToShow,
+        getColorSnackBar: state => state.colorSnackBar
     },
-    SET_CURRENT_PROJECT(state, currentProject) {
-      state.currentProject = currentProject
+    mutations: {
+        /* Project */
+        ADD_PROJECT(state, newProject) {
+            state.listProjects.push(newProject)
+            state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+        },
+        DELETE_PROJECT(state, project) {
+            state.listProjects.splice(state.listProjects.indexOf(project), 1)
+            console.log(state.listProjects)
+        },
+        SET_CURRENT_PROJECT(state, currentProject) {
+            state.currentProject = currentProject
+        },
+        SET_LIST_PROJECTS(state, list) {
+            state.listProjects = list
+            state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+        },
+        SET_EXPANSION_BAR_VISIBILITY(state, visibility) {
+            state.expansionBarVisibility = visibility
+        },
+        SET_MSG_SNACKBAR(state, msgSnackBar) {
+            state.msgSnackBar = msgSnackBar
+        },
+        SET_COLOR_SNACKBAR(state, colorSnackBar) {
+            state.colorSnackBar = colorSnackBar
+        },
+        SET_SNAKBAR_TO_SHOW(state, snackBarToShow) {
+            state.snackBarToShow = snackBarToShow
+        }
+
     },
-    SET_LIST_PROJECTS(state, list) {
-      state.listProjects = list
-      state.listProjects.sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1)
+    actions: {
+        addProject({
+            commit
+        }, project) {
+            commit('ADD_PROJECT', project)
+        },
+        deleteProject({
+            commit
+        }, project) {
+            commit('DELETE_PROJECT', project)
+        },
+        setCurrentProject({
+            commit
+        }, project) {
+            commit('SET_CURRENT_PROJECT', project)
+        },
+        setListProjects({
+            commit
+        }, list) {
+            commit('SET_LIST_PROJECTS', list)
+        },
+        setExpansionBarVisibility({
+            commit
+        }, visibility) {
+            commit('SET_EXPANSION_BAR_VISIBILITY', visibility)
+        },
+        setMsgSnackBar({
+            commit
+        }, msgSnackBar) {
+            commit('SET_MSG_SNACKBAR', msgSnackBar)
+        },
+        setSnackBarToShow({
+            commit
+        }, snackBarToShow) {
+            commit('SET_SNAKBAR_TO_SHOW', snackBarToShow)
+        },
+        setColorSnackBar({
+            commit
+        }, colorSnackBar) {
+            commit('SET_COLOR_SNACKBAR', colorSnackBar)
+        }
     },
-    SET_EXPANSION_BAR_VISIBILITY(state, visibility) {
-      state.expansionBarVisibility = visibility
-    }
-  },
-  actions: {
-    addProject({
-      commit
-    }, project) {
-      commit('ADD_PROJECT', project)
-    },
-    deleteProject({
-      commit
-    }, project) {
-      commit('DELETE_PROJECT', project)
-    },
-    setCurrentProject({
-      commit
-    }, project) {
-      commit('SET_CURRENT_PROJECT', project)
-    },
-    setListProjects({
-      commit
-    }, list) {
-      commit('SET_LIST_PROJECTS', list)
-    },
-    setExpansionBarVisibility({
-      commit
-    }, visibility) {
-      commit('SET_EXPANSION_BAR_VISIBILITY', visibility)
-    }
-  },
-  modules: {}
+    modules: {}
 })
